76112e12b125fb9874ab87b16b5375eda4c54933
Sameer <sameer.writes.70@gmail.com>
2022-08-26 21:34:48 +0530

    recipe

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ var buttonClass = document.querySelector('.buttonClass');
 var container = document.querySelector('.container');
 
 buttonClass.addEventListener('click', function(event) {
-            // container.innerHTML = "";
-            fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputClass.value}
+    // container.innerHTML = "";
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputClass.value}
     `)
-                .then(result => result.json())
-                .then(data => {
-                    // console.log(data);
-                    const food = data["meals"]
-                    food.map((meal) => {
-                        const div = document.createElement('div')
-                        div.innerHTML = `${meal.strMeal}${meal.strInstructions}`
-                        cont.append(div);
+        .then(result => result.json())
+        .then(data => {
+            // console.log(data);
+            const food = data["meals"]
+            food.map((meal) => {
+                const div = document.createElement('div')
+                div.innerHTML = `${meal.strMeal}${meal.strInstructions}`
+                cont.append(div);
 
-                    });
-                }).catch(err => alert("error"))
-                // })
+            });
+        }).catch(err => alert("error"))
+})
