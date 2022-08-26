3a9cc55f73df97f6f3308c6c4fcc55e469a26e0a
Sameer <sameer.writes.70@gmail.com>
2022-08-26 21:34:04 +0530

    recipe

--- script.js
+++ script.js
@@ -3,19 +3,18 @@ var buttonClass = document.querySelector('.buttonClass');
 var container = document.querySelector('.container');
 
 buttonClass.addEventListener('click', function(event) {
-        // container.innerHTML = "";
-        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputClass.value}
+            // container.innerHTML = "";
+            fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputClass.value}
     `)
-            .then(result => result.json())
-            .then(data => {
-                // console.log(data);
-                let { strMeal, strMealThumb, strInstructions, strYoutube } = element;
+                .then(result => result.json())
+                .then(data => {
+                    // console.log(data);
+                    const food = data["meals"]
+                    food.map((meal) => {
+                        const div = document.createElement('div')
+                        div.innerHTML = `${meal.strMeal}${meal.strInstructions}`
+                        cont.append(div);
 
-                let html = `<div class="main">
-            <h1>${strMeal}</h1> 
-            <p>${strInstructions}</p></div>`
-
-            });
-        container.innerHTML += html
-    }).catch(err => alert("error"))
-    // })
+                    });
+                }).catch(err => alert("error"))
+                // })
